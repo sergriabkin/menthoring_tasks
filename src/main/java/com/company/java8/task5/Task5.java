d75248d1ab05e81abfc190eed00d55378f4b8de2
Serhii_Riabkin <serhii_riabkin@epam.com>
2020-04-27 14:21:11 +0300

    Task 5 done

--- src/main/java/com/company/java8/task5/Task5.java
+++ src/main/java/com/company/java8/task5/Task5.java
@@ -1,11 +1,13 @@
 package com.company.java8.task5;
 
 import com.company.java8.Task;
+import com.company.java8.task5.domain.Author;
 import com.company.java8.task5.domain.Book;
 import com.company.java8.task5.repository.BookRepository;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,11 +24,16 @@ public class Task5 implements Task {
         System.out.println("3.3. filter books with only single author: ");
         System.out.println(subTask33(books));
         System.out.println("3.4. sort the books by number of pages/title: ");
+        System.out.println(subTask34(books));
         System.out.println("3.5. get list of all titles: ");
+        System.out.println(subTask35(books));
         System.out.println("3.6. print them using forEach method: ");
+        subTask36(books);
         System.out.println("3.7. get distinct list of all authors: ");
-
+        System.out.println(subTask37(books));
         System.out.println("7. Use the Optional type for determining the title of the biggest book of some author: ");
+        Author author2 = books[3].getAuthors().get(0);
+        System.out.println(subTask7(books, author2));
     }
 
     private boolean subTask31(Book[] books) {
@@ -51,5 +58,35 @@ public class Task5 implements Task {
                 .collect(Collectors.toList());
     }
 
+    private List<Book> subTask34(Book[] books) {
+        return Stream.of(books)
+                .sorted(Comparator.comparingInt(Book::getNumberOfPages).thenComparing(Book::getTitle))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> subTask35(Book[] books) {
+        return Stream.of(books)
+                .map(Book::getTitle)
+                .collect(Collectors.toList());
+    }
+
+    private void subTask36(Book[] books) {
+        subTask35(books).forEach(System.out::println);
+    }
+
+    private List<Author> subTask37(Book[] books) {
+        return Stream.of(books)
+                .flatMap(book -> book.getAuthors().stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private Optional<String> subTask7(Book[] books, Author author) {
+        return Stream.of(books)
+                .filter(book -> book.getAuthors().contains(author))
+                .sorted(Comparator.comparingInt(Book::getNumberOfPages).reversed())
+                .map(Book::getTitle)
+                .findFirst();
+    }
 
 }
